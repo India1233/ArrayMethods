040b50cdd43ccf06c630d0c9d4df4744d2713265
Suresh Shiga <suresh.shiga@indigo.co.in>
2021-02-25 22:52:00 +0530

    Add getDataMethod

--- ArrayMethods/ViewController.swift
+++ ArrayMethods/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         return String(string.reversed())
     }
     
+    func getDataFromString(name: String) -> String {
+        return name
+    }
+    
     let message = "Hello Git!"
     
     
@@ -26,6 +30,8 @@ class ViewController: UIViewController {
         
         print(message)
         print(reverse(string: "reverse"))
+        
+        print(getDataFromString(name: "Suresh"))
         
         let isValue = linearSearch(for:10, array: [1,2,3,4,5,6,7,8,9,10])
         print(isValue)
